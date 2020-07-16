994acb7b93510c579f6076597f7fec5eabfaaf08
gurux13 <gurux13@gmail.com>
2020-07-16 23:39:43 +0100

    Actual code for the stupid vue/flask integration

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 print('starting...')
-@app.route('/')
+@app.route('/io')
 def hello_world():
     return render_template('test_io.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 @socketio.on('message')
 def handle_message(message):
