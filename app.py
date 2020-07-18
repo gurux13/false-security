8961483ff8707ac824a95c529991ea08242a133d
ankineri <ankineri@gmail.com>
2020-07-18 22:19:17 +0100

    Game select screen: intermediate

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_socketio import SocketIO, emit
 
 from mod_gameselect.controller import mod_gameselect
 
-
 app = Flask(__name__)
 # TODO: Replace with server-side configuration
 app.config['SECRET_KEY'] = 'secret!'
@@ -13,9 +12,12 @@ app.register_blueprint(mod_gameselect)
 
 socketio = SocketIO(app)
 print('starting...')
+
+
 @app.route('/io')
 def hello_world():
     return render_template('test_io.html')
+
 
 @app.route('/')
 def index():
@@ -26,11 +28,14 @@ def index():
 def handle_message(message):
     print('received message: ' + message)
 
+
 @socketio.on('my event')
 def handle_my_custom_event(arg1):
     print('received args: ' + str(arg1))
 
+
 count = 0
+
 
 @socketio.on('clicked')
 def handle_my_custom_event2():
@@ -39,6 +44,6 @@ def handle_my_custom_event2():
     emit('upd', count, broadcast=True)
     print('sent')
 
+
 if __name__ == '__main__':
     socketio.run(app)
-
--- mod_gameselect/controller.py
+++ mod_gameselect/controller.py
@@ -1,13 +1,32 @@
 from flask import request, render_template, \
-                  flash, g, session, redirect, url_for
+    flash, g, session, redirect, url_for
 
 from flask import Blueprint
 from session import SessionKeys, SessionHelper
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 mod_gameselect = Blueprint('gameselect', __name__)
 
 
-@mod_gameselect.route('/')
+class GameForm(FlaskForm):
+    action = StringField(validators=[DataRequired()], _name='action')
+    game_key = StringField(validators=[DataRequired()], _name='game_key')
+
+
+def on_form(form):
+    if (form.validate()):
+        return "Ok"
+    else:
+        return "Fail"
+
+
+@mod_gameselect.route('/', methods=['GET', 'POST'])
 def index():
+    form = GameForm()
+    if form.validate_on_submit():
+        on_form(form)
     game_key = SessionHelper.get(SessionKeys.GAME_KEY, '')
     g.game_key = game_key
-    return render_template("index.html")
+    return render_template("index.html", form)
