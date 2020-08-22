073c5d49f0f59837b23874950c602478205d7e89
gurux13 <gurux13@gmail.com>
2020-08-22 21:15:07 +0300

    Minor design changes

--- utils/g_helper.py
+++ utils/g_helper.py
@@ -1,6 +1,7 @@
 from flask import g
 
 from globals import db
+from logic.game_logic import GameLogic
 from logic.player_manager import PlayerManager
 from session import SessionHelper, SessionKeys
 
@@ -9,6 +10,5 @@ def set_current_player():
     if SessionHelper.has(SessionKeys.PLAYER_ID):
         pm = PlayerManager(db)
         g.current_player = pm.get_my_player()
-
-
-
+        if g.current_player is not None:
+            g.current_game = GameLogic(db, g.current_player.model.game)
