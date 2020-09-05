7f4f3b468576d4cfb34d3713d2ab196882233fa9
gurux13 <gurux13@gmail.com>
2020-09-05 23:40:48 +0300

    Card component

--- static/mod_game/game.js
+++ static/mod_game/game.js
@@ -17,4 +17,53 @@ Vue.component('player-icon', {
     `,
 });
 
+Vue.component('card', {
+    props: [
+        'cards',
+        'card_id',
+        'type',
+        'cls',
+    ],
+    data: function() {
+        let type_cls = "";
+        switch (this.type) {
+            case 'off':
+                type_cls = 'card_offence';
+                break;
+            case 'acc':
+                type_cls = 'card_accident';
+                break;
+            case 'def':
+                type_cls = 'card_defence';
+                break;
+        }
+        return {
+            css_cls: type_cls + ' ' + this.cls + ' card'
+        }
+    },
+    computed: {
+        card: function () {
+            return this.cards[this.card_id];
+        },
+
+    },
+    methods: {
+        get_card: function(card_id) {
+            return this.cards[card_id];
+        }
+    },
+    template: `
+    <div :class="css_cls">
+        <p>
+            <b>{{card.name}}</b>
+            {{card.damage}}&nbsp;<span class="falsic"></span></p>
+        <div v-if="card.off_against">
+            <div class="against" v-for="elem in card.off_against" :key="elem.id">
+                {{get_card(elem.other_card).name}} -{{elem.value}}<br/>
+            </div>
+        </div>
+    </div>
+`
+});
+
 
