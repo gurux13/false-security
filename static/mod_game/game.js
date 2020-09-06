19608a689b29146fcb50806e18d78b56fc020f96
gurux13 <gurux13@gmail.com>
2020-09-06 17:33:22 +0300

    Defence cards look better

--- static/mod_game/game.js
+++ static/mod_game/game.js
@@ -23,6 +23,8 @@ Vue.component('card', {
         'card_id',
         'type',
         'cls',
+        'tiny',
+        'vs',
     ],
     data: function () {
         let type_cls = "";
@@ -46,7 +48,16 @@ Vue.component('card', {
             console.log("MASTER CARD", this.card_id);
             return this.cards[this.card_id];
         },
-
+        def_value: function () {
+            console.log("DEF AGAINST", this.card.def_against);
+            return this.card.def_against.filter((x) => x.other_card == this.vs)[0].value;
+        },
+        dmg_value: function () {
+            if (this.type == 'def') {
+                return this.def_value;
+            }
+            return this.card.damage;
+        }
     },
     methods: {
         get_card: function (card_id) {
@@ -57,15 +68,26 @@ Vue.component('card', {
     template: `
     <div :class="css_cls">
         <div class="card_falsics">
-            {{card.damage}}<span class="falsic"></span>
+            {{dmg_value}}<span class="falsic"></span>
         </div>
         <p>
-            <b>{{card.name}}</b></p>
-                    <div v-if="card.off_against">
+            <b>{{card.name}}</b>
+        </p>
+        <div v-if="card.off_against">
             <div class="against" v-for="elem in card.off_against" :key="elem.id">
                 {{get_card(elem.other_card).name}} -{{elem.value}}<br/>
             </div>
         </div>
+        <div v-if="card.def_against && !tiny">
+            <div class="against" v-for="elem in card.def_against" :key="elem.id">
+                {{get_card(elem.other_card).name}} +{{elem.value}}<br/>
+            </div>
+        </div>     
+<!--        <div v-if="vs && tiny">-->
+<!--            <div class="against" >-->
+<!--                {{get_card(vs).name}} +{{def_value}}<br/>-->
+<!--            </div>-->
+<!--        </div>             -->
     </div>
 `
 });
