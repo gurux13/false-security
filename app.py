57f40a1a2a0735c2440b5e3021ff7e210f47dcab
gurux13 <gurux13@gmail.com>
2020-09-09 10:28:04 +0300

    Support both mysql and sqlite; minor ui changes.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import g, Flask, render_template, json
+from flask import Flask, render_template
 
 from mod_test.test import mod_test
-from session import SessionHelper, SessionKeys
 
 if __name__ != '__main__':
-    import sys, os
+    import sys
+    import os
     if os.path.split(sys.argv[0])[1].replace('.py', '') == __name__:
         raise ImportError('No. Please do not import app. It will lead too all kinds of ridiculous stuff.')
 
@@ -14,7 +14,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 # install libmysqlclient-dev!
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:Ey*zM5a1v4cu@localhost/fs?charset=utf8mb4'
+if False:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:Ey*zM5a1v4cu@localhost/fs?charset=utf8mb4'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 import globals
@@ -34,9 +37,6 @@ def rules():
 
 if __name__ == '__main__':
     # Registering socketio listeners
-    import mod_game.waitroom
-    import mod_game.game_state
-    import connection_events
     from mod_gameselect.controller import mod_gameselect
 
     app.register_blueprint(mod_gameselect)
